f24ffc02a40f7aeed8b529624117f3fe326bb4ff
vrajmodi2004 <163124062+vrajmodi2004@users.noreply.github.com>
2024-03-12 16:22:22 +0530

    Add files via upload

---
+++ Exp_3/InsertionSort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int N = 10;
+    int arr[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = i; // for best  case
+                    // arr[i] = N - i ;      // for worst case
+    }
+
+    clock_t t = clock();
+
+    insertionSort(arr, N);
+    printArray(arr, N);
+
+    t = clock() - t;
+
+    float time_taken = (float)t / CLOCKS_PER_SEC;
+    cout << endl
+         << "Time taken by algoridhm is :"
+         << time_taken << endl;
+
+    return 0;
+}
